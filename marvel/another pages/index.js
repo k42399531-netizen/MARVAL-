ab3e7f04fe1f668c923da50eb2d16f7807b4a244
k42399531-netizen <k42399531@gmail.com>
2026-01-30 14:25:55 +0200

    Add files via upload

--- marvel/another pages/index.js
+++ marvel/another pages/index.js
@@ -7,53 +7,51 @@ const films = [
     {title: "هالك الخارق (2008)", link: "https://w10.mycima.cc/watch.php?vid=f9937c550", categry: "ا"},
     {title: "ثور (2011)", link: "https://w.cimalight.co/watch.php?vid=c002431d2", categry: "ه"},
     {title: "المنتقمون (2012)", link: "https://v5.shahidmosalsalat.business/play.php?vid=6Db5fab7d", categry: "ث"},
-    {title: "ثور: العالم المظلم (2013)", link: "https://w7.my-cima.net/watch.php?vid=446227653", categry: "ه"},
-    {title: "كابتن أمريكا: جندي الشتاء (2014)", link: "https://w7.my-cima.net/watch.php?vid=010bd002c", categry: "ك"},
-    {title: "حراس المجرة (2014)", link: "https://w7.my-cima.net/watch.php?vid=195679f22", categry: "ح"},
-    {title: "المنتقمون: عصر ألترون (2015)", link: "https://w7.my-cima.net/watch.php?vid=282069c9b", categry: "ث"},
-    {title: "الرجل النملة (2015)", link: "https://w7.my-cima.net/watch.php?vid=52706859e", categry: "ال"},
-    {title: "كابتن أمريكا: الحرب الأهلية (2016)", link: "https://w7.my-cima.net/watch.php?vid=00366c255", categry: "ك"},
-    {title: "دكتور سترينج (2016)", link: "https://w7.my-cima.net/watch.php?vid=8128331d2", categry: "د"},
-    {title: "حراس المجرة 2 (2017)", link: "https://w7.my-cima.net/watch.php?vid=e54f7a550", categry: "ح"},
-    {title: "سبايدرمان: العودة للوطن (2017)", link: "https://w7.my-cima.net/watch.php?vid=69366c255", categry: "س"},
-    {title: "ثور: راجناروك (2017)", link: "https://w7.my-cima.net/watch.php?vid=77366c255", categry: "ه"},
-    {title: "النمر الأسود (2018)", link: "https://w7.my-cima.net/watch.php?vid=11366c255", categry: "ن"},
-    {title: "المنتقمون: الحرب اللانهائية (2018)", link: "https://w7.my-cima.net/watch.php?vid=22366c255", categry: "ث"},
-    {title: "الرجل النملة والدبورة (2018)", link: "https://w7.my-cima.net/watch.php?vid=33366c255", categry: "ال"},
-    {title: "المنتقمون: نهاية اللعبة (2019)", link: "https://w7.my-cima.net/watch.php?vid=44366c255", categry: "ث"},
-    {title: "سبايدرمان: بعيدًا عن الوطن (2019)", link: "https://w7.my-cima.net/watch.php?vid=55366c255", categry: "س"},
-    {title: "الأرملة السوداء (2021)", link: "https://w7.my-cima.net/watch.php?vid=66366c255", categry: "أ"},
-    {title: "شانج شي وأسطورة الحلقات العشر (2021)", link: "https://w7.my-cima.net/watch.php?vid=77366c255", categry: "ش"},
-    {title: "الأبديون (2021)", link: "https://w7.my-cima.net/watch.php?vid=88366c255", categry: "ا"},
-    {title: "سبايدرمان: لا عودة للديار (2021)", link: "https://w7.my-cima.net/watch.php?vid=99366c255", categry: "س"},
-    {title: "دكتور سترينج في عوالم الجنون (2022)", link: "https://w7.my-cima.net/watch.php?vid=10366c255", categry: "د"},
-    {title: "ثور: الحب والرعد (2022)", link: "https://w7.my-cima.net/watch.php?vid=11466c255", categry: "ه"},
-    {title: "النمر الأسود: واكاندا للأبد (2022)", link: "https://w7.my-cima.net/watch.php?vid=11566c255", categry: "ن"},
-    {title: "الرجل النملة والدبورة: كوانتمانيا (2023)", link: "https://w7.my-cima.net/watch.php?vid=11666c255", categry: "ال"},
-    {title: "حراس المجرة 3 (2023)", link: "https://w7.my-cima.net/watch.php?vid=11766c255", categry: "ح"},
-    {title: "ذا مارفلز (2023)", link: "#", categry: "ا"},
-    {title: "ديدبول ووولفرين (2024)", link: "https://w7.my-cima.net/watch.php?vid=99c759495", categry: "د"},
-    {title: "كابتن أمريكا: عالم جديد شجاع (2025)", link: "#", categry: "ك"},
-    {title: "ثاندر بولتز (2025)", link: "#", categry: "ث"},
-    {title: "الرائعون الأربعة (2025)", link: "#", categry: "ا"},
-    {title: "بليد (2025)", link: "#", categry: "ب"},
+    {title: "ثور: العالم المظلم (2013)", link: "https://w10.mycima.cc/watch.php?vid=ff25d246e", categry: "ه"},
+    {title: "كابتن أمريكا: جندي الشتاء (2014)", link: "https://topcinema.rip/%D9%81%D9%8A%D9%84%D9%85-captain-america-the-winter-soldier-2014-%D9%85%D8%AA%D8%B1%D8%AC%D9%85-%D8%A7%D9%88%D9%86-%D9%84%D8%A7%D9%8A%D9%86/", categry: "ك"},
+    {title: "حراس المجرة (2014)", link: "https://w.cimalight.co/watch.php?vid=bc0d4f47f"},
+    {title: "المنتقمون: عصر ألترون (2015)", link: "https://x7k9f.sbs/%D8%B4%D8%A7%D9%87%D8%AF-%D9%81%D9%8A%D9%84%D9%85-avengers-age-of-ultron-2015-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/", categry: "ث"},
+    {title: "الرجل النملة (2015)", link: "https://w.cimalight.co/watch.php?vid=25fe8ff14"},
+    {title: "كابتن أمريكا: الحرب الأهلية (2016)", link: "https://topcinema.rip/%D9%81%D9%8A%D9%84%D9%85-captain-america-civil-war-2016-%D9%85%D8%AA%D8%B1%D8%AC%D9%85-%D8%A7%D9%88%D9%86-%D9%84%D8%A7%D9%8A%D9%86/"},
+    {title: "دكتور سترينج (2016)", link: "https://w.cimalight.co/watch.php?vid=fac203e01", categry: "د"},
+    {title: "حراس المجرة 2 (2017)", link: "https://w.cimalight.co/watch.php?vid=f49bef55c"},
+    {title: "سبايدرمان: العودة للوطن (2017)", link: "https://w.cimalight.co/watch.php?vid=8a1c0da49"},
+    {title: "ثور: راجناروك (2017)", link: "https://w10.mycima.cc/watch.php?vid=9bd3f263f"},
+    {title: "النمر الأسود (2018)", link: "https://w10.mycima.cc/watch.php?vid=dbd05215d", categry: "ن"},
+    {title: "المنتقمون: الحرب اللانهائية (2018)", link: "https://topcinema.rip/%D9%81%D9%8A%D9%84%D9%85-avengers-infinity-war-2018-%D9%85%D8%AA%D8%B1%D8%AC%D9%85-%D8%A7%D9%88%D9%86-%D9%84%D8%A7%D9%8A%D9%86/", categry: "ث"},
+    {title: "الرجل النملة والدبورة (2018)", link: "https://w.cimalight.co/watch.php?vid=9d5f8a71b", categry: "ال"},
+    {title: "المنتقمون: نهاية اللعبة (2019)", link: "https://topcinema.rip/%d9%81%d9%8a%d9%84%d9%85-avengers-endgame-2019-%d9%85%d8%aa%d8%b1%d8%ac%d9%85-%d8%a7%d9%88%d9%86-%d9%84%d8%a7%d9%8a%d9%86/watch/", categry: "ث"},
+    {title: "سبايدرمان: بعيدًا عن الوطن (2019)", link: "https://w10.mycima.cc/watch.php?vid=577e152b3", categry: "س"},
+    {title: "الأرملة السوداء (2021)", link: "https://tupcinema.media/watch/%D9%81%D9%8A%D9%84%D9%85-black-widow-2021-%D9%85%D8%AA%D8%B1%D8%AC%D9%85-%D8%A7%D9%88%D9%86-%D9%84%D8%A7%D9%8A%D9%86", categry: "أ"},
+    {title: "شي شينج شي واسطورة الحاقات العشر", link: "https://topcinema.rip/%d9%81%d9%8a%d9%84%d9%85-shang-chi-and-the-legend-of-the-ten-rings-2021-%d9%85%d8%aa%d8%b1%d8%ac%d9%85-%d8%a7%d9%88%d9%86-%d9%84%d8%a7%d9%8a%d9%86/watch/", categry: "ش"},
+    {title: "الأبديون (2021)", link: "https://w7.shahidwbas.tv/watch.php?vid=d41e5fe90", categry: "ا"},
+    {title: "سبايدرمان: لا عودة للديار (2021)", link: "https://topcinema.rip/%d9%81%d9%8a%d9%84%d9%85-spider-man-no-way-home-2021-%d9%85%d8%aa%d8%b1%d8%ac%d9%85-%d8%a7%d9%88%d9%86-%d9%84%d8%a7%d9%8a%d9%86/watch/", categry: "س"},
+    {title: "دكتور سترينج في عوالم الجنون (2022)", link: "https://w7.my-cima.net/watch.php?vid=da1f7ea14", categry: "د"},
+    {title: "ثور: الحب والرعد (2022)", link: "https://w10.mycima.cc/watch.php?vid=bef30395d", categry: "ه"},
+    {title: "النمر الأسود: واكاندا للأبد (2022)", link: "https://w.cimalight.co/watch.php?vid=964f2a122", categry: "ن"},
+    {title: "الرجل النملة والدبورة: كوانتمانيا (2023)", link: "https://w.cimalight.co/watch.php?vid=6a3199812", categry: "ال"},
+    {title: "حراس المجرة 3 (2023)", link: "https://w.cimalight.co/watch.php?vid=d2819438c", categry: "ح"},
+    {title: "ذا مارفلز (2023)", link: "https://w10.mycima.cc/watch.php?vid=1942e8363", categry: "ا"},
+    {title: "ديدبول ووولفرين (2024)", link: "https://w7.my-cima.net/watch.php?vid=e921c1c83", categry: "د"},
+    {title: "كابتن أمريكا: عالم جديد شجاع (2025)", link: "https://w.cimalight.co/watch.php?vid=59781d8e8", categry: "ك"},
+    {title: "ثاندر بولتز (2025)", link: "https://my-cima.club/watch.php?vid=82c366e1a", categry: "ث"},
+    {title: "الرائعون الأربعة (2025)", link: "https://topcinema.rip/%D9%81%D9%8A%D9%84%D9%85-the-fantastic-four-first-steps-2025-%D9%85%D8%AA%D8%B1%D8%AC%D9%85-%D8%A7%D9%88%D9%86-%D9%84%D8%A7%D9%8A%D9%86/", categry: "ا"},
     {title: "المنتقمون: يوم القيامة (2026)", link: "#", categry: "ث"},
     {title: "المنتقمون: الحروب السرية (2027)", link: "#", categry: "ث"},
-    {title: "رجال-إكس الأصول: وولفرين", link: "https://w.cimalight.co/watch.php?vid=62066c255", categry: "ا"},
-    {title: "رجال-إكس", link: "https://w.cimalight.co/watch.php?vid=77366c255", categry: "ا"},
-    {title: "رجال-إكس 2", link: "https://w.cimalight.co/watch.php?vid=11366c255", categry: "ا"},
-    {title: "رجال-إكس: الموقف الأخير", link: "https://w.cimalight.co/watch.php?vid=11366c255", categry: "ا"},
-    {title: "وولفرين", link: "https://w.cimalight.co/watch.php?vid=11366c255", categry: "ا"},
-    {title: "رجال-إكس: الفئة الأولى", link: "https://w.cimalight.co/watch.php?vid=11366c255", categry: "ا"},
-    {title: "رجال-إكس: أيام المستقبل الماضي", link: "https://w.cimalight.co/watch.php?vid=11366c255", categry: "ا"},
-    {title: "رجال-إكس: نهاية العالم (أبوكليبس)", link: "https://w.cimalight.co/watch.php?vid=11366c255", categry: "ا"},
-    {title: "لوجان", link: "https://w.cimalight.co/watch.php?vid=11366c255", categry: "ا"},
-    {title: "عنقاء الظلام", link: "https://w.cimalight.co/watch.php?vid=11366c255", categry: "ا"},
-    {title: "ديرديفيل", link: "https://w7.my-cima.net/watch.php?vid=66699c255", categry: "د"},
-    {title: "جيسيكا جونز", link: "https://w7.my-cima.net/watch.php?vid=77799c255", categry: "ج"},
-    {title: "لوك كيج", link: "https://w7.my-cima.net/watch.php?vid=88899c255", categry: "ل"},
-    {title: "القبضة الحديدية", link: "https://w7.my-cima.net/watch.php?vid=99999c255", categry: "أ"},
-    {title: "المدافعون", link: "https://w7.my-cima.net/watch.php?vid=00099c255", categry: "ا"},
+    {title: "رجال-إكس الأصول: وولفرين", link: "https://x7k9f.sbs/%D9%85%D8%B4%D8%A7%D9%87%D8%AF%D8%A9-%D9%81%D9%8A%D9%84%D9%85-x-men-origins-wolverine-2009-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/", categry: "ا"},
+    {title: "رجال-إكس 2", link: "https://x7k9f.sbs/%D9%85%D8%B4%D8%A7%D9%87%D8%AF%D8%A9-%D9%81%D9%8A%D9%84%D9%85-x-men-2-2003-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/", categry: "ا"},
+    {title: "رجال-إكس: الموقف الأخير", link: "https://x7k9f.sbs/%D9%85%D8%B4%D8%A7%D9%87%D8%AF%D8%A9-%D9%81%D9%8A%D9%84%D9%85-x-men-2-2003-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/", categry: "ا"},
+    {title: "وولفرين", link: "https://elcinema.com/work/1999288/", categry: "ا"},
+    {title: "رجال-إكس: الفئة الأولى", link: "https://x7k9f.sbs/%D9%85%D8%B4%D8%A7%D9%87%D8%AF%D8%A9-%D9%81%D9%8A%D9%84%D9%85-x-men-first-class-2011-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/", categry: "ا"},
+    {title: "رجال-إكس: أيام المستقبل الماضي", link: "https://x7k9f.sbs/%D9%85%D8%B4%D8%A7%D9%87%D8%AF%D8%A9-%D9%81%D9%8A%D9%84%D9%85-x-men-days-future-past-2014-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/", categry: "ا"},
+    {title: "رجال-إكس: نهاية العالم (أبوكليبس)", link: "https://x7k9f.sbs/%D9%85%D8%B4%D8%A7%D9%87%D8%AF%D8%A9-%D9%81%D9%8A%D9%84%D9%85-x-men-apocalypse-2016-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/", categry: "ا"},
+    {title: "لوجان", link: "https://x7k9f.sbs/%D9%85%D8%B4%D8%A7%D9%87%D8%AF%D8%A9-%D9%81%D9%8A%D9%84%D9%85-logan-2017-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/", categry: "ا"},
+    {title: "عنقاء الظلام", link: "https://x7k9f.sbs/%D9%85%D8%B4%D8%A7%D9%87%D8%AF%D8%A9-%D9%81%D9%8A%D9%84%D9%85-dark-phoenix-2019-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/", categry: "ا"},
+    {title: "ديرديفيل", link: "https://topcinema.rip/series/%D9%85%D8%B3%D9%84%D8%B3%D9%84-daredevil-%D8%A7%D9%84%D9%85%D9%88%D8%B3%D9%85-%D8%A7%D9%84%D8%AB%D8%A7%D9%86%D9%8A-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/", categry: "د"},
+    {title: "جيسيكا جونز", link: "https://moviz-time.live/series/%D9%85%D8%B3%D9%84%D8%B3%D9%84-jessica-jones-%D9%85%D8%AA%D8%B1%D8%AC%D9%85-%D8%A7%D9%84%D9%85%D9%88%D8%B3%D9%85-%D8%A7%D9%84%D8%A3%D9%88%D9%84/", categry: "ج"},
+    {title: "لوك كيج", link: "https://topcinema.rip/series/%D9%85%D8%B3%D9%84%D8%B3%D9%84-luke-cage-%D8%A7%D9%84%D9%85%D9%88%D8%B3%D9%85-%D8%A7%D9%84%D8%A7%D9%88%D9%84-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/", categry: "ل"},
+    {title: "القبضة الحديدية", link: "https://topcinema.rip/series/%D9%85%D8%B3%D9%84%D8%B3%D9%84-iron-fist-%D8%A7%D9%84%D9%85%D9%88%D8%B3%D9%85-%D8%A7%D9%84%D8%A7%D9%88%D9%84-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/", categry: "أ"},
+    {title: "المدافعون", link: "https://topcinema.rip/series/%D9%85%D8%B3%D9%84%D8%B3%D9%84-the-defenders-%D8%A7%D9%84%D9%85%D9%88%D8%B3%D9%85-%D8%A7%D9%84%D8%A7%D9%88%D9%84-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/", categry: "ا"},
     {title: "واندا فيجن", link: "https://topcinema.rip/series/%D9%85%D8%B3%D9%84%D8%B3%D9%84-wandavision-%D8%A7%D9%84%D9%85%D9%88%D8%B3%D9%85-%D8%A7%D9%84%D8%A7%D9%88%D9%84-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/"},
     {title: "الفالكون وجندي الشتاء", link: "https://x7k9f.sbs/season/%D9%85%D8%B3%D9%84%D8%B3%D9%84-the-falcon-and-the-winter-soldier-2021-%D9%85%D8%AA%D8%B1%D8%AC%D9%85-%D9%83%D8%A7%D9%85%D9%84/"},
     {title: "لوكي", link: "https://topcinema.rip/series/%D9%85%D8%B3%D9%84%D8%B3%D9%84-loki-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/"},
@@ -62,30 +60,30 @@ const films = [
     {title: "ميس مارفل", link: "https://topcinema.rip/series/%D9%85%D8%B3%D9%84%D8%B3%D9%84-ms-marvel-%D8%A7%D9%84%D9%85%D9%88%D8%B3%D9%85-%D8%A7%D9%84%D8%A7%D9%88%D9%84-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/"},
     {title: "شي هالك", link: "https://topcinema.rip/series/%D9%85%D8%B3%D9%84%D8%B3%D9%84-she-hulk-attorney-at-law-%D8%A7%D9%84%D9%85%D9%88%D8%B3%D9%85-%D8%A7%D9%84%D8%A7%D9%88%D9%84-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/"},
     {title: "الغزو السري", link: "https://topcinema.rip/series/%D9%85%D8%B3%D9%84%D8%B3%D9%84-secret-invasion-%D8%A7%D9%84%D9%85%D9%88%D8%B3%D9%85-%D8%A7%D9%84%D8%A7%D9%88%D9%84-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/"},
-    {title: "إيكو", link: "https://topcinema.rip/series/%D9%85%D8%B3%D9%84%D8%B3%D9%84-echo-%D8%A7%D9%84%D9%85%D9%88%D8%B3%D9%85-%D8%A7%D9%84%D8%A7%D9%88%D9%84-%D9%85%D8%AA%D8%B1%D8%AC%D9%85-%D8%A7%D9%88%D9%86-%D9%84%D8%A7%D9%8اي%D9%86/"},
+    {title: "إيكو", link: "https://x7k9f.sbs/season/%D9%85%D8%B3%D9%84%D8%B3%D9%84-l-echo-2024-%D9%83%D8%A7%D9%85%D9%84-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/"},
     {title: "أجاثا على طول", link: "https://topcinema.rip/series/%D9%85%D8%B3%D9%84%D8%B3%D9%84-agatha-all-along-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/"},
     {title: "عملاء شيلد", link: "https://topcinema.rip/series/%D9%85%D8%B3%D9%84%D8%B3%D9%84-agatha-all-along-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/"},
     {title: "العميلة كارتر", link: "https://topcinema.rip/series/%D9%85%D8%B3%D9%84%D8%B3%D9%84-agent-carter-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/"},
     {title: "الهاربون", link: "https://topcinema.rip/series/%D9%85%D8%B3%D9%84%D8%B3%D9%84-runaways-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/"}
 ];
 const financial_filmes = [
-    {name:"المنتقمون: لعبة النهاية (Endgame)",link:"#",text:"2.799 مليار $"},
-    {name:"المنتقمون: الحرب اللانهائية (Infinity War)",link:"#",text:"2.048 مليار $"},
-    {name:"المنتقمون (The Avengers)",link:"#",text:"1.518 مليار $"},
-    {name:"المنتقمون: عصر ألترون (Age of Ultron)",link:"#",text:"1.405 مليار $"},
-    {name:"النمر الأسود (Black Panther)",link:"#",text:"1.347 مليار $"},
-    {name:"آيرون مان 3 (Iron Man 3)",link:"#",text:"1.214 مليار $"},
-    {name:"كابتن أمريكا: الحرب الأهلية (Civil War)",link:"#",text:"1.153 مليار $"}
+    {name:"المنتقمون: لعبة النهاية (Endgame)",link:"https://w7.my-cima.net/watch.php?vid=c7da7ef21",text:"2.799 مليار $"},
+    {name:"المنتقمون: الحرب اللانهائية (Infinity War)",link:"https://w.cimalight.co/watch.php?vid=79e328ab3",text:"2.048 مليار $"},
+    {name:"المنتقمون (The Avengers)",link:"https://w.cimalight.co/watch.php?vid=096049a2b",text:"1.518 مليار $"},
+    {name:"المنتقمون: عصر ألترون (Age of Ultron)",link:"https://w.cimalight.co/watch.php?vid=994746ae0",text:"1.405 مليار $"},
+    {name:"النمر الأسود (Black Panther)",link:"https://w10.mycima.cc/watch.php?vid=963d8676c",text:"1.347 مليار $"},
+    {name:"آيرون مان 3 (Iron Man 3)",link:"https://w.cimalight.co/watch.php?vid=601c2f159",text:"1.214 مليار $"},
+    {name:"كابتن أمريكا: الحرب الأهلية (Civil War)",link:"https://topcinema.rip/%D9%81%D9%8A%D9%84%D9%85-captain-america-civil-war-2016-%D9%85%D8%AA%D8%B1%D8%AC%D9%85-%D8%A7%D9%88%D9%86-%D9%84%D8%A7%D9%8A%D9%86/",text:"1.153 مليار $"}
 ]
 
 const top_filmes = [
-    {title:"المنتقمون: الحرب اللانهائية (Infinity War)",link:"#",text:"8.4"},
-    {title:"المنتقمون: لعبة النهاية (Endgame)",link:"#",text:"8.4"},
-    {title:"سبايدرمان: لا عودة للديار (No Way Home)",link:"#",text:"8.2"},
-    {title:"حراس المجرة (Guardians of the Galaxy)",link:"#",text:"8.0"},
-    {title:"آيرون مان (Iron Man)",link:"#",text:"7.9"},
-    {title:"ثور: راجناروك (Thor: Ragnarok)",link:"#",text:"7.9"},
-    {title:"لوجان (Logan)",link:"#",text:"8.1"}
+    {title:"المنتقمون: الحرب اللانهائية (Infinity War)",link:"https://w.cimalight.co/watch.php?vid=79e328ab3",text:"8.4"},
+    {title:"المنتقمون: لعبة النهاية (Endgame)",link:"https://w7.my-cima.net/watch.php?vid=c7da7ef21",text:"8.4"},
+    {title:"سبايدرمان: لا عودة للديار (No Way Home)",link:"https://w.cimalight.co/watch.php?vid=5aa20c278",text:"8.2"},
+    {title:"حراس المجرة (Guardians of the Galaxy)",link:"https://topcinema.rip/assemblies/%D8%B3%D9%84%D8%B3%D9%84%D8%A9-%D8%A7%D9%81%D9%84%D8%A7%D9%85-%D8%AD%D8%B1%D8%A7%D8%B3-%D8%A7%D9%84%D9%85%D8%AC%D8%B1%D8%A9-guardians-of-the-galaxy-%D9%85%D8%AA%D8%B1%D8%AC%D9%85%D8%A9/",text:"8.0"},
+    {title:"آيرون مان (Iron Man)",link:"https://w.cimalight.co/watch.php?vid=20553c1e2",text:"7.9"},
+    {title:"ثور: راجناروك (Thor: Ragnarok)",link:"https://w10.mycima.cc/watch.php?vid=9bd3f263f",text:"7.9"},
+    {title:"لوجان (Logan)",link:"https://x7k9f.sbs/%D9%85%D8%B4%D8%A7%D9%87%D8%AF%D8%A9-%D9%81%D9%8A%D9%84%D9%85-logan-2017-%D9%85%D8%AA%D8%B1%D8%AC%D9%85/",text:"8.1"}
 ]
 
 // dom
@@ -100,7 +98,6 @@ for (let film of films) {
  let printer = `<div class="movie-card"><a href="${link}" target="_blank">${title}</a></div>`
  if (main) main.innerHTML += printer
 }
-
 for (let film of financial_filmes) {
  let name1 = film.name
  let link = film.link
@@ -108,8 +105,6 @@ for (let film of financial_filmes) {
  let printer = `<div class="movie-card financial"><a href="${link}" target="_blank">${name1}</a><p class="revenue">${text}</p></div>`
  if (financial_main) financial_main.innerHTML += printer
 }
-
-// السطر 85: تم إصلاح الخطأ بإضافة شرط (if (film)) للتأكد من وجود البيانات
 for (let film of top_filmes) {
  if (film && film.title) { 
     let title = film.title
